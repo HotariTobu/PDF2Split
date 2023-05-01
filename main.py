baace06f45aa7e73be3a8cbb7599d9b809f46f36
Hotari <hotari24tools@gmail.com>
2023-05-01 16:44:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,30 @@ import pypdf
 from copy import copy
 from pathlib import Path
 
+# ページを分割する処理
+def split(page):
+    # ページのサイズを取得する
+    page_width = page.mediabox.width
+    page_height = page.mediabox.height
+
+    # 範囲を求める
+    if page_width < page_height:
+        half_height = page_height / 2
+        rect1 = pypdf.generic.RectangleObject((0, half_height, page_width, page_height))
+        rect2 = pypdf.generic.RectangleObject((0, 0, page_width, half_height))
+    else:
+        half_width = page_width / 2
+        rect1 = pypdf.generic.RectangleObject((0, 0, half_width, page_height))
+        rect2 = pypdf.generic.RectangleObject((half_width, 0, page_width, page_height))
+
+    # 範囲を適用する
+    half1 = copy(page)
+    half1.cropbox = rect1
+    half2 = copy(page)
+    half2.cropbox = rect2
+
+    return half1, half2
+
 # ファイルを変換する処理
 # 成功したらTrue、それ以外はFalseを返す
 def process(input_file_path: str, output_file_path: str):
@@ -23,29 +47,32 @@ def process(input_file_path: str, output_file_path: str):
     # pypdfのPdfFileWriterオブジェクトを作成する
     pdf_writer = pypdf.PdfWriter()
 
+    # ページ数を取得する
+    page_num = len(pdf_reader.pages)
+
     # 各ページを2分割して、新しいPDFに追加する
-    for page in pdf_reader.pages:
-        # ページのサイズを取得する
-        page_width = page.mediabox.width
-        page_height = page.mediabox.height
+    for page_index in range(1, page_num, 2):
+        # ページを取得する
+        page1 = pdf_reader.pages[page_index - 1]
+        page2 = pdf_reader.pages[page_index]
 
-        # ページを2分割する
-        if page_width < page_height:
-            half_height = page_height / 2
-            rect1 = pypdf.generic.RectangleObject((0, half_height, page_width, page_height))
-            rect2 = pypdf.generic.RectangleObject((0, 0, page_width, half_height))
-        else:
-            half_width = page_width / 2
-            rect1 = pypdf.generic.RectangleObject((0, 0, half_width, page_height))
-            rect2 = pypdf.generic.RectangleObject((half_width, 0, page_width, page_height))
+        half1, half2 = split(page1)
+        half3, half4 = split(page2)
 
-        half1 = page
-        half1.cropbox = rect1
-        half2 = copy(page)
-        half2.cropbox = rect2
-
-        # 2つの半分のページを新しいPDFに追加する
+        # ページを新しいPDFに追加する
         pdf_writer.add_page(half1)
+        pdf_writer.add_page(half4)
+        pdf_writer.add_page(half2)
+        pdf_writer.add_page(half3)
+
+    if page_num % 2 == 1:
+        # 最後のページを取得する
+        last_page = pdf_reader.pages[page_num - 1]
+
+        half1, half2 = split(last_page)
+
+        pdf_writer.add_page(half1)
+        pdf_writer.add_blank_page(half1)
         pdf_writer.add_page(half2)
 
     # ファイルを閉じる
